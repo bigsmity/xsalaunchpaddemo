2b64b668d6d6b4590bd2b9e943d6c79d73c33720
Bradley Smith <brad_smiths_msg@hotmail.com>
2020-02-03 16:13:36 +1000

    initial upload

---
+++ launchpaddemoapp/launchpaddemoapp/resources/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("launchpaddemoapp.launchpaddemoapp.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
